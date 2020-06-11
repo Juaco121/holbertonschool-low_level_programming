b019a7470fbef6f7ed1d9e88468c4a52727d5412
Juaco121 <juanfernandogranada@gmail.com>
2020-06-11 20:22:13 +0000

    Add newline character at end of program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 /**
  *main - Entry point
  *
- *Description: This program will take a random number and decide whether the
- *last digit is 0, greater than 5, or less than 6 and not 0.
- *Return: Returns 0 upon successful completion.
+ *Description: This program will take a random number and display whether it is
+ *positive, negative, or zero.
+ *Return: Returns 0 upon successful run.
  */
 int main(void)
 {
@@ -15,12 +15,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if ((n % 10) == 0)
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	else if ((n % 10) > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n < 0)
+		printf("%d is negative", n);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+		printf("%d is zero", n);
 
+	putchar('\n');
 	return (0);
 }
