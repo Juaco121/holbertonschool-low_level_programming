faac317f5d383a997f3f4a220a10b33121747433
Juaco121 <juanfernandogranada@gmail.com>
2020-06-18 16:20:34 +0000

    function that checks for uppercase character.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,12 @@
  */
 int _isupper(int c)
 {
-    if (c >= 0x41 && c <= 0x5A)
-    {
-        return (1);
-    }
-    else 
-    {
-        return (0);
-    }
+  if (c >= 0x41 && c <= 0x5A)
+  {
+    return (1);
+  }
+  else 
+  {
+    return (0);
+  }
 }
