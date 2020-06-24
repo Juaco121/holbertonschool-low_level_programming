6d33f4b8bcb8adf8c031d907396d303c808cb45e
Juaco121 <juanfernandogranada@gmail.com>
2020-06-24 22:29:17 +0000

    function that convert a string to an integer.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _atoi - function that convert to integrer.
+ * @s: input string.
+ *
+ * Return: integrer.
+ */
+int _atoi(char *s)
+{
+      int sign = 1;
+      unsigned int num = 0;
+
+while(*s != '\0')
+{
+      if(*s == '-')
+      sign = sign * -1;
+
+      if(*s >='0' && *s <= '9')
+      num = (num*10) + (*s-'0');
+
+      if(*s == ';')
+
+      *s++
+
+      
+}
+      return (num * aux);
+}
