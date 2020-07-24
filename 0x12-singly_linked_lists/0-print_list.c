c93482204d24b610ffda099caa901377a35b2216
Juaco121 <juanfernandogranada@gmail.com>
2020-07-24 00:51:44 +0000

    Problem with betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,17 +13,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-			h = h->next;
-			elements++;
-		}
 		else
-		{
-		printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		elements++;
-		}
 	}
 	return (elements);
 }
